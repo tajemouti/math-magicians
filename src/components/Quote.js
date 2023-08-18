20c937573e1f629d3d900e306eac83c7c2e0911a
tajemouti <tajemouti@gmail.com>
2023-08-18 15:18:58 +0100

    Fetch data inside Quote component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,16 +1,38 @@
 import React, { useState, useEffect } from 'react';
 
 const Quote = () => {
-  const [quote, setQuote] = useState('');
+  const [quote, setQuote] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
   useEffect(() => {
+    const apiKey = 'O6pDj1/4tagnkcW1489qjw==98lkKoKn9t6If37D';
+    const category = 'inspirational';
 
+    fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+      headers: {
+        'X-Api-Key': apiKey,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const randomQuote = data[Math.floor(Math.random() * data.length)];
+        setQuote(randomQuote);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
   }, []);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div className="loading">Loading...</div>;
   }
 
   if (error) {
@@ -24,6 +46,7 @@ const Quote = () => {
 
   return (
     <div className="quote">
+      <h2>Inspirational Quote</h2>
       <blockquote>{quote.quote}</blockquote>
       <p>
         -
