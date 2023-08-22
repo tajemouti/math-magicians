19aa41df869553ac940ff1e6d4ad7d97943d7f14
tajemouti <tajemouti@gmail.com>
2023-08-22 21:48:22 +0100

    Add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter as Router, Route, Routes, Link,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quote from './components/Quote';
 import './App.css';
 
 const App = () => (
   <div className="app">
-    <Calculator />
+    <nav className="navbar">
+      <h1 className="navbar-title">Math Magicians</h1>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/calculator">Calculator</Link>
+        </li>
+        <li>
+          <Link to="/quote">Quote</Link>
+        </li>
+      </ul>
+    </nav>
   </div>
 );
 
