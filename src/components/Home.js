9cd3cf44df771eb7918e8177d714b3adb01a2eac
tajemouti <tajemouti@gmail.com>
2023-08-22 21:39:24 +0100

    Add home page component

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h2>Welcome to our page!</h2>
+    <p>Math Magicians – your gateway to seamless calculations and a touch of inspiration!</p>
+    <p>With a sleek design and friendly UI, our app transforms complex math into a breeze.</p>
+    <p>From basic arithmetic to an advanced feature such as modulo operations.</p>
+    <p>Enjoy thought-provoking quotes for that extra motivation.</p>
+    <p>Embrace your inner math magician today – let&apos;s make math fun!</p>
+  </div>
+);
+
+export default Home;
