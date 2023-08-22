55afd13229c89ad43767be7e4c257eb45be3686e
tajemouti <tajemouti@gmail.com>
2023-08-22 21:52:38 +0100

    Route component page

--- src/App.js
+++ src/App.js
@@ -8,22 +8,29 @@ import Quote from './components/Quote';
 import './App.css';
 
 const App = () => (
-  <div className="app">
-    <nav className="navbar">
-      <h1 className="navbar-title">Math Magicians</h1>
-      <ul className="navbar-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/calculator">Calculator</Link>
-        </li>
-        <li>
-          <Link to="/quote">Quote</Link>
-        </li>
-      </ul>
-    </nav>
-  </div>
+  <Router>
+    <div className="app">
+      <nav className="navbar">
+        <h1 className="navbar-title">Math Magicians</h1>
+        <ul className="navbar-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/quote">Quote</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </div>
+  </Router>
 );
 
 export default App;
