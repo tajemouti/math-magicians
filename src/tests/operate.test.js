9fa76c7de13596dda635839293fca443cc9b38c3
firstpj <114886262+firstpj@users.noreply.github.com>
2023-08-23 22:34:19 +0100

    add more unit tests to operate.js

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -4,4 +4,20 @@ describe('operate function', () => {
   it('should perform addition correctly', () => {
     expect(operate(10, 20, '+')).toBe('30');
   });
+
+  it('should perform subtraction correctly', () => {
+    expect(operate(50, 25, '-')).toBe('25');
+  });
+
+  it('should perform multiplication correctly', () => {
+    expect(operate(5, 8, 'x')).toBe('40');
+  });
+
+  it('should perform division correctly', () => {
+    expect(operate(100, 5, '÷')).toBe('20');
+  });
+
+  it('should handle division by zero', () => {
+    expect(operate(10, 0, '÷')).toBe('Cannot divide by zero');
+  });
 });
